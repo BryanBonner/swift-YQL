c1e4d14eeaf3c7103ee805f1c33ffa3af01ad6a2
David McLaren <sirusblk@gmail.com>
2017-04-02 13:04:03 -0700

    Added YQL class implementation in Swift

---
+++ swift-YQL/YQL.swift
@@ -0,0 +1,34 @@
+//
+//  YQL.swift
+//  CurrencyExchange
+//
+//  Created by David McLaren on 4/1/17.
+//  Copyright © 2017 David McLaren. All rights reserved.
+//
+
+import Foundation
+
+class YQL {
+    private let QUERY_PREFIX = "http://query.yahooapis.com/v1/public/yql?q="
+    private let QUERY_SUFFIX = "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
+    
+    public func query(_ statement:String, completion: @escaping ([String: Any]) -> Void) {
+        let url = URL(string: "\(QUERY_PREFIX)\(statement.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!)\(QUERY_SUFFIX)")
+        
+        let task = URLSession.shared.dataTask(with: url!) {
+            (data, response, error) in
+            if error != nil {
+                print(error!)
+            } else {
+                do {
+                    let dataDict = try JSONSerialization.jsonObject(with: data!, options: []) as? [String: Any]
+                    let results:[String: Any] = dataDict!
+                    completion(results)
+                } catch {
+                    print(error)
+                }
+            }
+        }
+        task.resume()
+    }
+}
