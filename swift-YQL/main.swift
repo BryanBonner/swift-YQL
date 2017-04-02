23a1ca3c9c557dfecc7cee870315fc6bcb58c0ef
David McLaren <sirusblk@gmail.com>
2017-04-02 13:04:35 -0700

    Added example usage of YQL class

--- swift-YQL/main.swift
+++ swift-YQL/main.swift
@@ -8,5 +8,21 @@
 
 import Foundation
 
-print("Hello, World!")
+// Example usage
+let myYQL = YQL()
+let queryString = "select * from yahoo.finance.quote where symbol in (\"YHOO\",\"AAPL\",\"GOOG\",\"MSFT\")"
 
+// Network session is asyncronous so use a closure to act upon data once data is returned
+myYQL.query(queryString) { jsonDict in
+    // With the resulting jsonDict, pull values out
+    // jsonDict["query"] results in an Any? object
+    // to extract data, cast to a new dictionary (or other data type)
+    // repeat this process to pull out more specific information
+    let queryDict = jsonDict["query"] as! [String: Any]
+    print(queryDict["count"]!)
+    print(queryDict["results"]!)
+}
+
+// Needed to let async operation finish
+// Could handle with semaphores or closures instead...
+RunLoop.main.run()
